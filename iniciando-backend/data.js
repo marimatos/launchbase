50c42dd07efe828b1ae2a7711e2648ef17ac962d
Marina <marina.msp@gmail.com>
2021-04-01 21:07:16 -0300

    utilizando dados dinamicos

---
+++ iniciando-backend/data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com Javascript",
+    duration: "1:40m",
+    price: "FREE",
+    featured: true
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com Python",
+    duration: "1:40m",
+    price: "FREE"
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com C++",
+    duration: "1:40m",
+    price: "FREE",
+    featured: true
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com Java",
+    duration: "1:40m",
+    price: "FREE"
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com .NET",
+    duration: "1:40m",
+    price: "FREE",
+    featured: true
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com C#",
+    duration: "1:40m",
+    price: "FREE"
+  }  
+]
--- iniciando-backend/server.js
+++ iniciando-backend/server.js
@@ -2,21 +2,54 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static("public"))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false,
+  noCache: true
 })
 
 server.get("/", function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars2.githubusercontent.com/u/45432113?s=460&u=061a17c1eb368b06944edbab236a5780cf68df6f&v=4",
+    name: "Marina Matos",
+    role: "Analista de Sistemas",
+    description: "Estudante de programação web",
+    links: [
+      {name: "Github", url: "https://github.com/marimatos"},
+      {name: "Linkedin", url: "https://www.linkedin.com/in/marinamatos/"},
+      {name: "FreeCodeCamp", url: "https://www.freecodecamp.org/marinamatos"}
+    ]
+
+  }
+
+  return res.render("about", {about})
 })
 
 server.get("/portfolio", function(req,res) {
-  return res.render("portfolio")
+  return res.render("portfolio", {items: videos})
+})
+
+server.get("/video", function(req, res) {
+  const id = req.query.id
+  const video = videos.find(function(video) {
+    return video.id == id
+  })
+
+  if (!video) {
+    return res.send("Video not found")
+  } else {
+    return res.render("video", { item:video })
+  }
+})
+
+server.use(function(req, res) {
+  res.status(404).render("not-found");
 })
 
 server.listen(5000, function () {
