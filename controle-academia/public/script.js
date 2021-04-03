19226eded0eed76c5c380a8046259d5a4ec34aac
Marina <marina.msp@gmail.com>
2021-04-03 14:09:50 -0300

    upload academia e aulas

---
+++ controle-academia/public/script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+  card.addEventListener('click', function(){
+    const videoId = card.getAttribute('id');
+    window.location.href = `/video?id=${videoId}`
+  })
+}
+
+
