566baac232a7c2b83d3802db99f8133503fc6141
Marina <marina.msp@gmail.com>
2021-04-10 19:24:27 -0300

    admin foodfy

--- foodfy/routes.js
+++ foodfy/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const site = require('./controllers/site')
+const recipes = require('./controllers/recipes')
 
 routes.get('/', function(req, res){
 	return res.redirect('/site');
@@ -11,5 +12,15 @@ routes.get('/site', site.index )
 routes.get('/site/about', site.about)
 routes.get('/site/recipes', site.recipes)
 routes.get("/site/recipes/:id", site.recipe)
+
+//admin
+routes.get("/admin/recipes", recipes.index ); //listagem de receitas
+//routes.get("/admin/recipes/create", recipes.create); // mostrar formulário de criação de receita
+//routes.get("/admin/recipes/:id", recipes.show); // mostrar detalhe
+//routes.get("/admin/recipes/:id/edit", recipes.edit); // mostrar edição de receita
+
+//routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+//routes.put("/admin/recipes", recipes.put); // Editar uma receita
+//routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 module.exports = routes
