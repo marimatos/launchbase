71a47a40cfbd35a088722c572d50e69e05381acd
Marina <marina.msp@gmail.com>
2021-04-12 22:07:47 -0300

    criação página edit foodfy

--- foodfy/controllers/recipes.js
+++ foodfy/controllers/recipes.js
@@ -3,3 +3,27 @@ const data = require('../data.json')
 exports.index = function(req, res){
   return res.render('admin/recipes', {recipes: data.recipes})
 }
+
+exports.show = function(req,res) {
+  const { id } = req.params
+
+  const foundRecipe = data.recipes.find(function(recipe){
+    return recipe.id == id
+  })
+
+  if (!foundRecipe) return res.send("Receita não encontrado")
+ 
+  return res.render("admin/recipe", {recipe: foundRecipe})
+}
+
+exports.edit = function(req,res) {
+  const { id } = req.params
+
+  const foundRecipe = data.recipes.find(function(recipe){
+    return recipe.id == id
+  })
+
+  if (!foundRecipe) return res.send("Receita não encontrado")  
+
+  return res.render("admin/edit", {recipe: foundRecipe})
+}
--- foodfy/routes.js
+++ foodfy/routes.js
@@ -16,8 +16,8 @@ routes.get("/site/recipes/:id", site.recipe)
 //admin
 routes.get("/admin/recipes", recipes.index ); //listagem de receitas
 //routes.get("/admin/recipes/create", recipes.create); // mostrar formulário de criação de receita
-//routes.get("/admin/recipes/:id", recipes.show); // mostrar detalhe
-//routes.get("/admin/recipes/:id/edit", recipes.edit); // mostrar edição de receita
+routes.get("/admin/recipes/:id", recipes.show); // mostrar detalhe
+routes.get("/admin/recipes/:id/edit", recipes.edit); // mostrar edição de receita
 
 //routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 //routes.put("/admin/recipes", recipes.put); // Editar uma receita
