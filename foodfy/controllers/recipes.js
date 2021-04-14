69ebf2dd8449c0b21745f6be50a1a0a4e7f1d076
Marina <marina.msp@gmail.com>
2021-04-13 23:22:03 -0300

    admin foodfy

--- foodfy/controllers/recipes.js
+++ foodfy/controllers/recipes.js
@@ -1,4 +1,5 @@
 const data = require('../data.json')
+const fs = require('fs')
 
 exports.index = function(req, res){
   return res.render('admin/recipes', {recipes: data.recipes})
@@ -27,3 +28,47 @@ exports.edit = function(req,res) {
 
   return res.render("admin/edit", {recipe: foundRecipe})
 }
+
+exports.create = function(req,res) {
+  return res.render('admin/create')
+}
+
+exports.delete = function(req,res) {
+  const {id} = req.body
+
+  const filteredRecipes = data.recipes.filter(function(recipe){
+    return recipe.id != id
+  })
+
+  data.recipes = filteredRecipes
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send('Write file error')
+
+    res.redirect('/admin/recipes')
+  })
+}
+
+exports.post = function(req,res) {
+  const keys = Object.keys(req.body)
+  //cria um array com as chaves do body
+
+     
+    let recipe = req.body
+    const id = Number(data.recipes.length + 1)
+  
+    data.recipes.push({
+      id,
+      ...recipe
+    })
+    
+  
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+      if(err) return res.send("Write file error!")
+  
+      return res.redirect("/admin/recipes")
+    })
+  
+    
+}
+  
+  
--- foodfy/public/script.js
+++ foodfy/public/script.js
@@ -18,6 +18,39 @@ for (let i = 0; i < description.length; i++) {
 }
 
 
+//Admin
+const addFields = {
+  input: "",
+  parent: "",
+  container: "",
 
+  add(event) {
+    addFields.input = event.target
+    addFields.parent = addFields.input.parentElement
+    addFields.container = addFields.parent.querySelector('.field-container').lastElementChild
+
+    const newField = addFields.container.cloneNode(true)
+
+    if (newField.children[0].value === '') return;
+
+    newField.children[0].value = '';
+    addFields.parent.querySelector('.field-container').appendChild(newField);
+  },
+  remove(event) {
+    addFields.input = event.target
+    addFields.parent = addFields.input.parentElement.parentElement
+
+    console.log(addFields.parent.parentElement.children);
+    
+
+    if (addFields.parent.parentElement.children.length > 1) {
+      if (addFields.parent.querySelector('input').value == "") {
+        return
+      } else {
+        addFields.parent.parentElement.removeChild(addFields.parent)
+      }
+    }
+  }
+}
 
 
